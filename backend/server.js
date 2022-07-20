97ce882efda6ad4d1ae3f1946e1ad9ba00b83dae
hyperkarmic <randy_pan@yahoo.com>
2022-07-20 23:55:37 +0100

    all the express-mongo stuff set up - on Vim

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 //express app
 const app = express();
@@ -13,15 +14,20 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/api/workouts", workoutRoutes);
-//
-//
-//
-//
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("conneccted to db & listening");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 //express app
 
 // routes
 app.use("api/workouts/", workoutRoutes);
-
-app.listen(process.env.PORT, () => {
-  console.log("listening on port4000");
-});
