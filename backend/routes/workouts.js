584de65f79b8c04b41db2e1c996e7031c88a1dca
hyperkarmic <randy_pan@yahoo.com>
2022-07-21 16:37:11 +0100

    first section of controllers set up

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,38 +1,26 @@
 const express = require("express");
-const Workout = require("../models/workoutModel");
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 //get all workourqts
-router.get("/", (req, res) => {
-  res.json({ mssg: "smells like ys sisters" });
-  //get a single workout
-  //
-  router.get("/:id", (req, res) => {
-    res.json({ mssg: "Get a single workout" });
-  });
-  //Post a new workout
-  router.post("/", async (req, res) => {
-    const { title, load, reps } = req.body;
+router.get("/", getWorkouts);
+//get a single workout
+//
+router.get("/:id", getWorkout);
+//Post a new workout
+router.post("/", createWorkout);
 
-    try {
-      const workout = await Workout.create({
-        title,
-        load,
-        reps,
-      });
-      res.status(200).json(workout);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  });
-  //delete a word
-  router.delete("/:id", (req, res) => {
-    res.json({ mssg: "DELETE a new workout" });
-  });
-  //update a record
-  router.patch("/:id", (req, res) => {
-    res.json({ mssg: "PATCH a new workout" });
-  });
+//delete a word
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "DELETE a new workout" });
+});
+//update a record
+router.patch("/:id", (req, res) => {
+  res.json({ mssg: "PATCH a new workout" });
 });
 
 module.exports = router;
