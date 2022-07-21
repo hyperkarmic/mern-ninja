057aa8470b10d6df7d7beef2b24958d71879fbcb
hyperkarmic <randy_pan@yahoo.com>
2022-07-21 14:15:45 +0100

    post route functionality set up and tested with NVIM

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Workout = require("../models/workoutModel");
 
 const router = express.Router();
 //get all workourqts
@@ -10,8 +11,19 @@ router.get("/", (req, res) => {
     res.json({ mssg: "Get a single workout" });
   });
   //Post a new workout
-  router.post("/", (req, res) => {
-    res.json({ mssg: "POST a new workout" });
+  router.post("/", async (req, res) => {
+    const { title, load, reps } = req.body;
+
+    try {
+      const workout = await Workout.create({
+        title,
+        load,
+        reps,
+      });
+      res.status(200).json(workout);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   });
   //delete a word
   router.delete("/:id", (req, res) => {
